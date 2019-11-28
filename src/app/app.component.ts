a57e19af385798951ee320a155924720d218d676
JP <jp@beyondlogical.com>
2019-11-27 21:13:34 -0500

    working example of adding an angular component dynamically

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, ComponentFactoryResolver, OnInit } from '@angular/core';
+import { HolderComponent } from './holder/holder.component';
+import { AddComponentDirective } from './add-component.directive';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'app';
+  @ViewChild(AddComponentDirective) appAddComponent: AddComponentDirective;
+  constructor(private componentFactoryResolver: ComponentFactoryResolver) {
+
+  }
+  loadComponent() {
+    const componentFactory = this.
+      componentFactoryResolver.resolveComponentFactory(HolderComponent);
+    this.appAddComponent.viewContainerRef.createComponent(componentFactory);
+
+  }
+  ngOnInit() {
+    this.loadComponent();
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,21 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { AddComponentDirective } from './add-component.directive';
+import { HolderComponent } from './holder/holder.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AddComponentDirective,
+    HolderComponent
   ],
   imports: [
     BrowserModule
   ],
   providers: [],
+  entryComponents: [HolderComponent],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
